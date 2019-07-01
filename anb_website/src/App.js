5bb97f2ee46f9ba7632bed0ec39344639f10b866
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-01 17:06:35 -0400

    project started

---
+++ anb_website/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./images/logo_small_white.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <p>Welcome to our Page! </p>
+    </div>
+  );
+}
+
+export default App;
