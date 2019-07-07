3188868efa674a56e6ad8bbfe52d3fb5055114ff
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-07 18:31:53 -0400

    added more pics, second carousel

---
+++ anb_website/src/data/snake_carousel.js
@@ -0,0 +1,7 @@
+import caramel_jag from "../images/snakes/caramel_jag.jpg";
+import jungle_beans from "../images/snakes/jungle_beans.jpg";
+import red_jaya from "../images/snakes/red_jaya.jpg";
+
+const images = [caramel_jag, jungle_beans, red_jaya];
+
+export default images;
