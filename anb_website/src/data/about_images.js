a183b7ff57c9179890515179861e38d6a60832f5
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-16 17:31:22 -0400

    resized carousel image files

--- anb_website/src/data/about_images.js
+++ anb_website/src/data/about_images.js
@@ -1,7 +1,7 @@
-import ben from "../images/people/ben.jpg";
-import addie from "../images/people/addie.jpg";
-import jesi from "../images/people/jesi1.JPG";
-import cody from "../images/people/cody.jpg";
+import ben from "../images/people/ben_600.jpg";
+import addie from "../images/people/addie_600.jpg";
+import jesi from "../images/people/jesi1_600.jpg";
+import cody from "../images/people/cody_600.jpg";
 
 const images = [ben, addie, jesi, cody];
 
--- anb_website/src/data/snake_carousel.js
+++ anb_website/src/data/snake_carousel.js
@@ -1,8 +1,8 @@
-import caramel_jag from "../images/snakes/caramel_jag.jpg";
-import donk_picky from "../images/snakes/donk_picky.jpg";
-import jungle3 from "../images/snakes/postings/jungle3.jpg";
-import papuan3 from "../images/snakes/postings/papuan3.jpg";
-import ben2 from "../images/people/ben2.JPG";
+import caramel_jag from "../images/snakes/caramel_jag_600.jpg";
+import donk_picky from "../images/snakes/donk_picky_600.jpg";
+import jungle3 from "../images/snakes/postings/jungle3_600.jpg";
+import papuan3 from "../images/snakes/postings/papuan3_600.jpg";
+import ben2 from "../images/people/ben2_600.jpg";
 
 const images = [jungle3, papuan3, caramel_jag, donk_picky, ben2];
 
