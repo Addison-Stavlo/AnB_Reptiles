32ee504da1e279a958e9654f805c2327a0ee8a85
Addison-Stavlo <AddieStavlo@gmail.com>
2019-07-28 01:24:49 -0400

    customized carousel more

--- anb_website/src/components/carousel/Carousel.js
+++ anb_website/src/components/carousel/Carousel.js
@@ -6,19 +6,34 @@ export default class Carousel extends React.Component {
     super(props);
     this.countDelay = 2500 + Math.random() * 1000;
     this.imageBox = React.createRef();
+    this.imageBox2 = React.createRef();
     this.state = {
-      count: 0
+      count: 0,
+      count2: 1
     };
   }
 
   componentDidMount() {
-    setInterval(this.increaseCount, this.countDelay);
+    // setInterval(this.increaseCount, this.countDelay * 2);
     this.imageBox.current.classList.add("transition");
+    this.imageBox2.current.classList.add("transition2");
+    setTimeout(() => {
+      setInterval(this.increaseCount, this.countDelay * 2);
+    }, (this.countDelay * 3) / 2);
+    setTimeout(() => {
+      setInterval(this.increaseCount2, this.countDelay * 2);
+    }, this.countDelay / 2);
   }
 
   increaseCount = () => {
     this.setState({
-      count: (this.state.count + 1) % this.props.images.length
+      count: (this.state.count + 2) % this.props.images.length
+    });
+  };
+
+  increaseCount2 = () => {
+    this.setState({
+      count2: (this.state.count2 + 2) % this.props.images.length
     });
   };
 
@@ -32,6 +47,12 @@ export default class Carousel extends React.Component {
           src={images[this.state.count]}
           alt="alternating images"
           ref={this.imageBox}
+        />
+        <img
+          className="image-main"
+          src={images[this.state.count2]}
+          alt="alternating images"
+          ref={this.imageBox2}
         />
       </Wrapper>
     );
@@ -52,11 +73,32 @@ const Wrapper = styled.div`
     10% {
       opacity: 1;
     }
-    70% {
+    50% {
       opacity: 1;
+    }
+    60% {
+      opacity: 0;
     }
     100% {
       opacity: 0;
+    }
+  }
+
+  @keyframes fadeSequence-secondary {
+    0% {
+      opacity: 1;
+    }
+    10% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 0;
+    }
+    60% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 1;
     }
   }
 
@@ -64,11 +106,16 @@ const Wrapper = styled.div`
     width: 300px;
     border-radius: 10px;
     box-shadow: 0 0 10px 0 black;
-    /* position: absolute; */
+    position: absolute;
   }
 
   .transition {
-    animation: fadeSequence-main ${props => `${props.countDelay / 1000}s`}
+    animation: fadeSequence-main ${props => `${(props.countDelay * 2) / 1000}s`}
       infinite;
+  }
+
+  .transition2 {
+    animation: fadeSequence-secondary
+      ${props => `${(props.countDelay * 2) / 1000}s`} infinite;
   }
 `;
