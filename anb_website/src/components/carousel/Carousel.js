2448b982a4e0ed12a2e6dac1b819d5539cf76409
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-23 16:28:35 -0400

    carousel update complete

--- anb_website/src/components/carousel/Carousel.js
+++ anb_website/src/components/carousel/Carousel.js
@@ -4,25 +4,16 @@ import styled from "styled-components";
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.countDelay = 1500 + Math.random() * 1000;
-    // this.imageBox = React.createRef();
-    // this.imageBox2 = React.createRef();
+    this.countDelay = 1500 + Math.random() * 1500;
     this.state = {
       count: 0
-      // count2: 1
     };
   }
 
   componentDidMount() {
-    // setInterval(this.increaseCount, this.countDelay * 2);
-    // this.imageBox.current.classList.add("transition");
-    // this.imageBox2.current.classList.add("transition2");
     setTimeout(() => {
       setInterval(this.increaseCount, this.countDelay * 2);
     }, this.countDelay);
-    // setTimeout(() => {
-    //   setInterval(this.increaseCount2, this.countDelay * 2);
-    // }, this.countDelay / 2);
   }
 
   increaseCount = () => {
@@ -31,36 +22,18 @@ export default class Carousel extends React.Component {
     });
   };
 
-  // increaseCount2 = () => {
-  //   this.setState({
-  //     count2: (this.state.count2 + 2) % this.props.images.length
-  //   });
-  // };
-
   render() {
     const images = this.props.images;
 
     return (
       <Wrapper countDelay={this.countDelay}>
-        {/* <img
-          className="image-main"
-          src={images[this.state.count]}
-          alt="alternating images"
-          ref={this.imageBox}
-        />
-        <img
-          className="image-main"
-          src={images[this.state.count2]}
-          alt="alternating images"
-          ref={this.imageBox2}
-        /> */}
         {images.map((image, index) => {
           let isActive = index === this.state.count % images.length;
           return (
             <img
               src={image}
               alt="snakes and peoples"
-              className={isActive ? "carousel-img active-img" : "carousel-img"}
+              className={isActive ? "active-img" : null}
             />
           );
         })}
@@ -76,57 +49,20 @@ const Wrapper = styled.div`
   align-items: center;
   position: relative;
 
-  /* @keyframes fadeSequence-main {
-    0% {
-      opacity: 0;
-    }
-    10% {
-      opacity: 1;
-    }
-    50% {
-      opacity: 1;
-    }
-    60% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 0;
-    }
-  } */
-
-  /* @keyframes fadeSequence-secondary {
-    0% {
-      opacity: 1;
-    }
-    10% {
-      opacity: 0;
-    }
-    50% {
-      opacity: 0;
-    }
-    60% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 1;
-    }
-  } */
-
   img {
     width: 300px;
     border-radius: 10px;
     box-shadow: 0 0 10px 0 black;
     position: absolute;
     animation-name: fadeout;
-    animation-duration: 3s;
+    animation-duration: 1.5s;
     animation-iteration-count: 1;
     opacity: 0;
   }
 
-
   .active-img {
     animation-name: fadein;
-    animation-duration: 3s;
+    animation-duration: 1.5s;
     animation-iteration-count: 1;
     opacity: 1;
   }
@@ -148,14 +84,4 @@ const Wrapper = styled.div`
       opacity: 1;
     }
   }
-
-  /* .transition {
-    animation: fadeSequence-main ${props => `${(props.countDelay * 2) / 1000}s`}
-      infinite;
-  }
-
-  .transition2 {
-    animation: fadeSequence-secondary
-      ${props => `${(props.countDelay * 2) / 1000}s`} infinite;
-  } */
 `;
